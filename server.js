48604935c336cf81f6ade35e897a8731a61957a4
Virupaxgouda . C . KumbarKattimani <virupax.work@gmail.com>
2021-05-07 18:21:35 -0400

    Revert "Added security using helmet"
    This reverts commit 922abf45bd4d301d3670b6c081fcd9bb3fe802a3.

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require('express'),
 	app = express(),
 	cors = require('cors'),
-    helmet = require("helmet"),
     apiConstants = require('./App Constants/apiConstants');
 	port = process.env.PORT || apiConstants.DEFAULT_PORT,
     newsController = require('./controllers/NewsController');
 
 app.use(cors());
-app.use(helmet());
 app.use('/api/v1/', newsController);
 
 
